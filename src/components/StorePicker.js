cec42a9e0a35589dfbdc81bb424fd8dca1f40c35
Frank Ruiz <frank.ruiz.limon@gmail.com>
2017-12-15 07:35:43 -0800

    set up onSubmit event handler for StorePicker component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,11 +2,27 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  goToStore(event) {
+    event.preventDefault();
+    console.log('change url');
+    // first grab the text from the box
+    console.log(this.storeInput.value);
+    // second were going to transition from / to /store/:storeId
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={e => this.goToStore(e)}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()} />
+        <input
+          type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+          ref={input => {
+            this.storeInput = input;
+          }}
+        />
         <button type="submit">Visit Store></button>
       </form>
     );
