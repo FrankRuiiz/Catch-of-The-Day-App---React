047431b5cedc63f22fbd2fb388b8adb5f6560730
Frank Ruiz <frank.ruiz.limon@gmail.com>
2017-12-20 05:26:46 -0800

    Add fishForm to Inventory component and initial state to App for fishes

--- src/components/App.js
+++ src/components/App.js
@@ -5,17 +5,38 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
-	render() {
-		return (
-			<div className="catch-of-the-day">
-				<div className="menu">
-					<Header tagline="Fresh Seafood Market" />
-				</div>
-				<Order />
-				<Inventory />
-			</div>
-		);
-	}
+  constructor() {
+    super();
+
+    this.addFish = this.addFish.bind(this);
+    // get initial state
+    this.state = {
+      fishes: {},
+      order: {}
+    };
+  }
+
+  addFish(fish) {
+    // update our state
+    const fishes = { ...this.state.fishes };
+    // add in ournew fish
+    const timestamp = Date.now();
+    fishes[`fish-${timestamp}`] = fish;
+    // set state
+    this.setState({ fishes: fishes });
+  }
+
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <Header tagline="Fresh Seafood Market" />
+        </div>
+        <Order />
+        <Inventory addFish={this.addFish} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import AddFishForm from './AddFishForm';
 
 class Inventory extends React.Component {
-	render() {
-		return (
-			<p>Inventory</p>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <p>Inventory</p>
+        <AddFishForm addFish={this.props.addFish} />
+      </div>
+    );
+  }
 }
 
 export default Inventory;
